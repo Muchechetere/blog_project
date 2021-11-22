e22cd91568d134ff48c5ddb9dbb2afd97fd748b6
Muchechetere <moyotutsirai@gmail.com>
2021-11-22 23:18:17 +0200

    Changes Made

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -8,5 +9,8 @@ class Post(models.Model):
 
     def __str___ (self):
         return self.title + '|' + str(self.author)
+    
+    def get_absolute_url(self):
+        return reverse('article-detail',args=(str(self.id)))
 
 # Create your models here.

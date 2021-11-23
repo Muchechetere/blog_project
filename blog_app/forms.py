9d080bffdf944359d0630a42d5802f45dddfe9ea
Muchechetere <moyotutsirai@gmail.com>
2021-11-23 10:33:41 +0200

    Edit and Delete functions added

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -11,3 +11,13 @@ class PostForm(forms.ModelForm):
             'author':forms.Select(attrs={'class' : 'form-control'}),
             'body':forms.Textarea(attrs={'class' : 'form-control'}),
         }
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','author','body')
+
+        widgets = {
+            'title':forms.TextInput(attrs={'class' : 'form-control'}),
+            'author':forms.Select(attrs={'class' : 'form-control'}),
+            'body':forms.Textarea(attrs={'class' : 'form-control'}),
+        }
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,9 @@
+from django.forms import fields
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm,EditForm
+from django.urls import reverse_lazy
 
 class HomeView(ListView):
     model = Post
@@ -15,4 +17,15 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
+    
    
